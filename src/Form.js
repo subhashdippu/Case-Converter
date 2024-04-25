c4c217594a1217aebba79cc84af524459f6ec0f6
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-04-25 12:56:44 +0530

    Second Commit

--- src/Form.js
+++ src/Form.js
@@ -3,10 +3,18 @@ import React, { useState } from 'react'
 export default function Form(props) {
     const handleUpClick = () => {
         console.log("Clicked Up")
-        setText("You clicked handleUpClick ")
+        let newText = text.toUpperCase()
+        // setText("You clicked handleUpClick ")
+        setText(newText)
+    }
+    const handleLowClick = () => {
+        console.log("Clicked Up")
+        let newText = text.toLowerCase()
+        // setText("You clicked handleUpClick ")
+        setText(newText)
     }
     const handleDelete = () => {
-        setText("")
+        setText(" ")
     }
     const handleOnChange = (event) => {
         console.log("OnChange")
@@ -14,14 +22,22 @@ export default function Form(props) {
     }
     const [text, setText] = useState("Enter the text")
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <label for="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+        <>
+            <div>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <label for="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+                </div>
+                <button className='btn btn-primary mx-4' onClick={handleUpClick}>UpperCase</button>
+                <button className='btn btn-primary mx-4' onClick={handleLowClick}>Lower Case</button>
+                <button type="button" class="btn btn-info mx-4" onClick={handleDelete}>Delete Content</button>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick}>Convert</button>
-            <button type="button" class="btn btn-info mx-4" onClick={handleDelete}>Delete Content</button>
-        </div>
+            <div className='container my-4'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split().length} word and {text.length} Characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes to read.</p>
+            </div>
+        </>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(props) {
     return (
         <div>
             <div classNameName="App">
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-black">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#">{props.title}</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
