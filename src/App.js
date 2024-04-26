46070047dcd2c891f3e690c4eb9b75db5d544f08
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-04-26 09:57:54 +0530

    Dark and Light mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
 import Form from './Form';
+import About from './About';
 function App() {
   return (
     <>
       <Navbar title='Navbar' />
-      <Form heading="Enter the text to modify" />
+      {/* <Form heading="Enter the text to modify" /> */}
+      <About />
     </>
   );
 }
